f0cff1fedbcf9aa81cab15df079ef157279f798c
Jonathan Gilbert <JonathanG@iQmetrix.com>
2020-06-11 22:03:20 -0500

    Release Tag 2.3-beta5

--- xunit.runner.visualstudio/VsTestRunner.cs
+++ xunit.runner.visualstudio/VsTestRunner.cs
@@ -18,7 +18,7 @@ using Microsoft.Extensions.DependencyModel;
 using System.Reflection;
 #endif
 
-namespace Xunit.Runner.VisualStudio.TestAdapter
+namespace Xunit.Runner.VisualStudio
 {
     [FileExtension(".appx")]
     [FileExtension(".dll")]
@@ -243,13 +243,16 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
                                                      TestAssemblyConfiguration configuration)
             where TVisitor : IVsDiscoverySink, IDisposable
         {
-            var reporterMessageHandler = GetRunnerReporter(new[] { assemblyFileName }).CreateMessageHandler(new VisualStudioRunnerLogger(logger));
-            var assembly = new XunitProjectAssembly { AssemblyFilename = assemblyFileName };
-            var fileName = Path.GetFileNameWithoutExtension(assemblyFileName);
+            if (cancelled)
+                return false;
+
+            var fileName = "(unknown assembly)";
+
             try
             {
-                if (cancelled)
-                    return false;
+                var reporterMessageHandler = GetRunnerReporter(new[] { assemblyFileName }).CreateMessageHandler(new VisualStudioRunnerLogger(logger));
+                var assembly = new XunitProjectAssembly { AssemblyFilename = assemblyFileName };
+                fileName = Path.GetFileNameWithoutExtension(assemblyFileName);
 
                 if (!IsXunitTestAssembly(assemblyFileName))
                 {
@@ -301,6 +304,7 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
                 else
                     logger.LogWarning("Exception discovering tests from {0}: {1}", fileName, ex);
             }
+
             return true;
         }
 
@@ -432,20 +436,22 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
             if (cancelled)
                 return;
 
-            var assembly = new XunitProjectAssembly { AssemblyFilename = runInfo.AssemblyFileName };
-            var assemblyFileName = runInfo.AssemblyFileName;
-            var assemblyDisplayName = Path.GetFileNameWithoutExtension(assemblyFileName);
-            var configuration = runInfo.Configuration;
-            var shadowCopy = configuration.ShadowCopyOrDefault;
-
-            var appDomain = assembly.Configuration.AppDomain ?? AppDomainDefaultBehavior;
-            var longRunningSeconds = assembly.Configuration.LongRunningTestSecondsOrDefault;
-
-            if (RunSettingsHelper.DisableAppDomain)
-                appDomain = AppDomainSupport.Denied;
+            var assemblyDisplayName = "(unknown assembly)";
 
             try
             {
+                var assembly = new XunitProjectAssembly { AssemblyFilename = runInfo.AssemblyFileName };
+                var assemblyFileName = runInfo.AssemblyFileName;
+                assemblyDisplayName = Path.GetFileNameWithoutExtension(assemblyFileName);
+                var configuration = runInfo.Configuration;
+                var shadowCopy = configuration.ShadowCopyOrDefault;
+
+                var appDomain = assembly.Configuration.AppDomain ?? AppDomainDefaultBehavior;
+                var longRunningSeconds = assembly.Configuration.LongRunningTestSecondsOrDefault;
+
+                if (RunSettingsHelper.DisableAppDomain)
+                    appDomain = AppDomainSupport.Denied;
+
 #if WINDOWS_UAP
                 // For AppX Apps, use the package location
                 assemblyFileName = Path.Combine(Windows.ApplicationModel.Package.Current.InstalledLocation.Path, Path.GetFileName(assemblyFileName));
@@ -473,7 +479,7 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
                                 assemblyDiscoveredInfo = new AssemblyDiscoveredInfo
                                 {
                                     AssemblyFileName = source,
-                                    DiscoveredTestCases = visitor.TestCases.Select(testCase => new DiscoveredTestCase(source, discoverer, testCase, logger, testPlatformContext)).ToList()
+                                    DiscoveredTestCases = GetVsTestCases(source, discoverer, visitor, logger, testPlatformContext)
                                 };
                             },
                             assemblyFileName,
@@ -495,11 +501,6 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
                         // Apply any filtering
                         var filter = new TestCaseFilter(runContext, logger, assemblyDiscoveredInfo.AssemblyFileName, traitNames);
                         var filteredTestCases = assemblyDiscoveredInfo.DiscoveredTestCases.Where(dtc => filter.MatchTestCase(dtc.VSTestCase)).ToList();
-
-                        // Force unique names if there is more than 1 testcase with the same name
-                        foreach (var groupWithDuplicateNames in filteredTestCases.GroupBy(dtc => dtc.Name).Where(group => group.Count() > 1))
-                            foreach (var discoveredTestCaseWithDuplicateName in groupWithDuplicateNames)
-                                discoveredTestCaseWithDuplicateName.ForceUniqueName();
 
                         foreach (var filteredTestCase in filteredTestCases)
                         {
@@ -642,9 +643,9 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
                         var ctor = type.DeclaredConstructors.FirstOrDefault(c => c.GetParameters().Length == 0);
                         if (ctor == null)
                         {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            ConsoleHelper.SetForegroundColor(ConsoleColor.Yellow);
                             Console.WriteLine($"Type {type.FullName} in assembly {assembly} appears to be a runner reporter, but does not have an empty constructor.");
-                            Console.ResetColor();
+                            ConsoleHelper.ResetColor();
                             continue;
                         }
 
@@ -698,9 +699,9 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
                     var ctor = type.GetConstructor(new Type[0]);
                     if (ctor == null)
                     {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        ConsoleHelper.SetForegroundColor(ConsoleColor.Yellow);
                         Console.WriteLine($"Type {type.FullName} in assembly {dllFile} appears to be a runner reporter, but does not have an empty constructor.");
-                        Console.ResetColor();
+                        ConsoleHelper.ResetColor();
                         continue;
                     }
 
@@ -728,6 +729,19 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
 #endif
         }
 
+        IList<DiscoveredTestCase> GetVsTestCases(string source, ITestFrameworkDiscoverer discoverer, VsExecutionDiscoverySink visitor, LoggerHelper logger, TestPlatformContext testPlatformContext)
+        {
+            var descriptorProvider = (discoverer as ITestCaseDescriptorProvider) ?? new DefaultTestCaseDescriptorProvider(discoverer);
+            var testCases = visitor.TestCases;
+            var descriptors = descriptorProvider.GetTestCaseDescriptors(testCases, false);
+            var results = new List<DiscoveredTestCase>(descriptors.Count);
+
+            for (int idx = 0; idx < descriptors.Count; ++idx)
+                results.Add(new DiscoveredTestCase(source, descriptors[idx], testCases[idx], logger, testPlatformContext));
+
+            return results;
+        }
+
         class AssemblyDiscoveredInfo
         {
             public string AssemblyFileName;
@@ -746,25 +760,14 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
 
             public string UniqueID { get; }
 
-            public DiscoveredTestCase(string source, ITestFrameworkDiscoverer discoverer, ITestCase testCase, LoggerHelper logger, TestPlatformContext testPlatformContext)
+            public DiscoveredTestCase(string source, TestCaseDescriptor descriptor, ITestCase testCase, LoggerHelper logger, TestPlatformContext testPlatformContext)
             {
-                var testMethod = testCase.TestMethod;
-                var testClassName = testMethod.TestClass.Class.Name;
-                var testMethodName = testMethod.Method.Name;
-
-                TraitNames = testCase.Traits.Keys;
-                VSTestCase = VsDiscoverySink.CreateVsTestCase(source, discoverer, testCase, false, logger, testPlatformContext);
-                Name = $"{testClassName}.{testMethodName}";
-
+                Name = $"{descriptor.ClassName}.{descriptor.MethodName} ({descriptor.UniqueID})";
                 TestCase = testCase;
-                UniqueID = testCase.UniqueID;
-
-                VSTestCase = VsDiscoverySink.CreateVsTestCase(source, discoverer, testCase, false, logger, testPlatformContext, testClassName, testMethodName, UniqueID);
-                TraitNames = VSTestCase.Traits.Select(x => x.Name);
+                UniqueID = descriptor.UniqueID;
+                VSTestCase = VsDiscoverySink.CreateVsTestCase(source, descriptor, logger, testPlatformContext);
+                TraitNames = descriptor.Traits.Keys;
             }
-
-            public void ForceUniqueName()
-                => VsDiscoverySink.ForceUniqueName(VSTestCase, UniqueID);
         }
     }
 }
