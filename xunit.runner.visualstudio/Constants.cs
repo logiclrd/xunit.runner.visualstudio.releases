61fb78ea0fd7415ef769a9c2b3c22abc24cf2de4
Jonathan Gilbert <JonathanG@iQmetrix.com>
2020-06-11 22:03:20 -0500

    Release Tag 2.3-beta4

--- xunit.runner.visualstudio/Constants.cs
+++ xunit.runner.visualstudio/Constants.cs
@@ -2,6 +2,12 @@
 {
     public static class Constants
     {
-        public const string ExecutorUri = "executor://xunit/VsTestRunner2";
+#if NET452 || WINDOWS_UAP
+        public const string ExecutorUri = "executor://xunit/VsTestRunner2/desktop_and_uap";
+#elif NETCOREAPP1_0
+        public const string ExecutorUri = "executor://xunit/VsTestRunner2/netcoreapp";
+#else
+#error Unknown target platform
+#endif
     }
 }
