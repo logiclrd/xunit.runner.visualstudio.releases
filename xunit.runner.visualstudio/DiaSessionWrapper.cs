50d3a3256138df08711795627d2f7abcb6b8373e
Jonathan Gilbert <JonathanG@iQmetrix.com>
2020-06-11 22:02:37 -0500

    Release Tag 1.9.2

--- xunit.runner.visualstudio/DiaSessionWrapper.cs
+++ xunit.runner.visualstudio/DiaSessionWrapper.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace Xunit.Runner.VisualStudio
@@ -8,7 +12,9 @@ namespace Xunit.Runner.VisualStudio
     // that the PDB file is missing or corrupt).
     public class DiaSessionWrapper : IDisposable
     {
+        AppDomain appDomain;
         string assemblyFilename;
+        DiaSessionWrapperHelper helper;
         DiaSession session;
         bool sessionHasErrors;
 
@@ -18,6 +24,45 @@ namespace Xunit.Runner.VisualStudio
 
             if (Environment.GetEnvironmentVariable("XUNIT_SKIP_DIA") != null)
                 sessionHasErrors = true;
+            else
+            {
+                try
+                {
+                    AppDomainSetup setup = new AppDomainSetup
+                    {
+                        ApplicationBase = Path.GetDirectoryName(new Uri(typeof(DiaSessionWrapperHelper).Assembly.CodeBase).LocalPath),
+                        ApplicationName = Guid.NewGuid().ToString(),
+                        LoaderOptimization = LoaderOptimization.MultiDomainHost,
+                        ShadowCopyFiles = "true",
+                    };
+
+                    setup.ShadowCopyDirectories = setup.ApplicationBase;
+                    setup.CachePath = Path.Combine(Path.GetTempPath(), setup.ApplicationName);
+
+                    appDomain = AppDomain.CreateDomain(setup.ApplicationName, null, setup, new PermissionSet(PermissionState.Unrestricted));
+
+                    helper = (DiaSessionWrapperHelper)appDomain.CreateInstanceAndUnwrap(
+                        assemblyName: typeof(DiaSessionWrapperHelper).Assembly.FullName,
+                        typeName: typeof(DiaSessionWrapperHelper).FullName,
+                        ignoreCase: false,
+                        bindingAttr: 0,
+                        binder: null,
+                        args: new[] { assemblyFilename },
+                        culture: null,
+                        activationAttributes: null,
+                        securityAttributes: null
+                    );
+                }
+                catch
+                {
+                    sessionHasErrors = true;
+                }
+            }
+        }
+
+        private MethodInfo GetMethod(string typeName, string methodName)
+        {
+            return null;
         }
 
         public DiaNavigationData GetNavigationData(string typeName, string methodName)
@@ -25,10 +70,10 @@ namespace Xunit.Runner.VisualStudio
             if (!sessionHasErrors)
                 try
                 {
+                    helper.Normalize(ref typeName, ref methodName);
+
                     if (session == null)
-                    {
                         session = new DiaSession(assemblyFilename);
-                    }
 
                     return session.GetNavigationData(typeName, methodName);
                 }
@@ -44,6 +89,9 @@ namespace Xunit.Runner.VisualStudio
         {
             if (session != null)
                 session.Dispose();
+
+            if (appDomain != null)
+                AppDomain.Unload(appDomain);
         }
     }
 }
